1600b93b7175e46ef1b656db341bf1fb5dda7f83
trent <tjurewicz@gmail.com>
2010-01-08 15:19:16 -0600

    modified tinymce/views.py with the filebrowser index url name

--- tinymce/views.py
+++ tinymce/views.py
@@ -123,6 +123,6 @@ def render_to_js_vardef(var_name, var_value):
 
 def filebrowser(request):
     fb_url = "%s://%s%s" % (request.is_secure() and 'https' or 'http',
-            request.get_host(), urlresolvers.reverse('filebrowser-index'))
+            request.get_host(), urlresolvers.reverse('fb_browse'))
     return render_to_response('tinymce/filebrowser.js', {'fb_url': fb_url},
             context_instance=RequestContext(request))
